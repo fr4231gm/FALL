336bbfe0b626d44c7de06d5653c0999e3dfb0632
Fran <fr4231gm@gmail.com>
2019-07-01 12:09:27 +0200

    inicializating project

---
+++ AcmeConference/src/main/java/controllers/WelcomeController.java
@@ -0,0 +1,69 @@
+/*
+ * WelcomeController.java
+ * 
+ * Copyright (C) 2019 Universidad de Sevilla
+ * 
+ * The use of this project is hereby constrained to the conditions of the
+ * TDG Licence, a copy of which you may download from
+ * http://www.tdg-seville.info/License.html
+ */
+
+package controllers;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import services.ActorService;
+import domain.Actor;
+
+@Controller
+@RequestMapping("/welcome")
+public class WelcomeController extends AbstractController {
+
+	// Constructors -----------------------------------------------------------
+
+	public WelcomeController() {
+		super();
+	}
+
+
+	//	@Autowired
+	private ActorService	actorService;
+
+
+	//
+	//	
+
+	// Index ------------------------------------------------------------------		
+
+	@RequestMapping(value = "/index")
+	public ModelAndView index(@RequestParam(required = false) String name, @RequestParam(required = false) final String title) {
+		ModelAndView result;
+		SimpleDateFormat formatter;
+		String moment;
+
+		try {
+			final Actor principal = this.actorService.findByPrincipal();
+			if (principal != null)
+				name = principal.getName();
+
+		} catch (final Throwable oops) {
+
+		}
+
+		formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		moment = formatter.format(new Date());
+
+		result = new ModelAndView("welcome/index");
+		result.addObject("name", name);
+		result.addObject("title", title);
+		result.addObject("moment", moment);
+
+		return result;
+	}
+}
---
+++ AcmeConference/src/main/java/security/LoginService.java
@@ -0,0 +1,98 @@
+/*
+ * LoginService.java
+ * 
+ * Copyright (C) 2019 Universidad de Sevilla
+ * 
+ * The use of this project is hereby constrained to the conditions of the
+ * TDG Licence, a copy of which you may download from
+ * http://www.tdg-seville.info/License.html
+ */
+
+package security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+@Service
+@Transactional
+public class LoginService implements UserDetailsService {
+
+	// Managed repository -----------------------------------------------------
+
+	@Autowired
+	UserAccountRepository	userRepository;
+
+
+	// Supporting services ----------------------------------------------------
+	//
+	//	@Autowired
+	//	private AdministratorService	administratorService;
+
+	// Business methods -------------------------------------------------------
+
+	@Override
+	public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+		Assert.notNull(username);
+
+		UserDetails result;
+
+		result = this.userRepository.findByUsername(username);
+		Assert.notNull(result);
+		// WARNING: The following sentences prevent lazy initialisation problems!
+		Assert.notNull(result.getAuthorities());
+		result.getAuthorities().size();
+
+		return result;
+	}
+
+	public static UserAccount getPrincipal() {
+		UserAccount result;
+		SecurityContext context;
+		Authentication authentication;
+		Object principal;
+
+		// If the asserts in this method fail, then you're
+		// likely to have your Tomcat's working directory
+		// corrupt. Please, clear your browser's cache, stop
+		// Tomcat, update your Maven's project configuration,
+		// clean your project, clean Tomcat's working directory,
+		// republish your project, and start it over.
+
+		context = SecurityContextHolder.getContext();
+		Assert.notNull(context);
+		authentication = context.getAuthentication();
+		Assert.notNull(authentication);
+		principal = authentication.getPrincipal();
+		Assert.isTrue(principal instanceof UserAccount);
+		result = (UserAccount) principal;
+		Assert.notNull(result);
+		Assert.isTrue(result.getId() != 0);
+
+		return result;
+	}
+
+	/*
+	 * Metodo utilizado para actualizar una user account. Sera utilizado para el metodo changeBan de AdministratorService
+	 */
+	//	public UserAccount update(final UserAccount ua) {
+	//		Administrator principal;
+	//		UserAccount result;
+	//
+	//		principal = this.administratorService.findByPrincipal();
+	//		Assert.notNull(principal);
+	//		Assert.notNull(ua);
+	//
+	//		result = this.userRepository.save(ua);
+	//
+	//		return result;
+	//	}
+
+}
