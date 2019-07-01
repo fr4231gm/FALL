921bffcfa69bde5b209f433c7ab89946f60e409e
Fran <fr4231gm@gmail.com>
2019-07-01 12:11:08 +0200

    adding script for bdd

--- AcmeConference.sql
+++ AcmeConference.sql
@@ -1,7 +1,7 @@
 # Borrar la BBDD
-drop database if exists `Acme-Rookies`;
+drop database if exists `Acme-Conference`;
 # Crear la BBDD
-create database `Acme-Rookies`;
+create database `Acme-Conference`;
 # Elimina al usuario acme-User
 drop user 'acme-user';
 # Crear al usuario acme-user
@@ -14,10 +14,10 @@ create user 'acme-manager'@'%'
  identified by password '*FDB8CD304EB2317D10C95D797A4BD7492560F55F';
 # Dar CRUD al usuario acme-user
 grant select, insert, update, delete
-	on `Acme-Rookies`.* to 'acme-user'@'%';
+	on `Acme-Conference`.* to 'acme-user'@'%';
 # Dar todos los permisos al usuario acme-manager
 grant select, insert, update, delete, create, drop,
 	references, index, alter, create temporary tables,
 	lock tables, create view, create routine,
 	alter routine, execute, trigger, show view
-	on `Acme-Rookies`.* to 'acme-manager'@'%';
+	on `Acme-Conference`.* to 'acme-manager'@'%';
