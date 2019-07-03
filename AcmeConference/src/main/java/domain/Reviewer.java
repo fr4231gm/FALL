d14b89da62f199e8676b56cd9635d10eae7317fa
Fran <fr4231gm@gmail.com>
2019-07-03 12:44:04 +0200

    fix - Actors and creditcard datatype

--- AcmeConference/src/main/java/domain/Reviewer.java
+++ AcmeConference/src/main/java/domain/Reviewer.java
@@ -1,43 +1,44 @@
+
 package domain;
 
-import javax.persistence.ManyToMany;
-import org.hibernate.validator.constraints.SafeHtml;
-import org.hibernate.validator.constraints.NotBlank;
 import java.util.Collection;
-import javax.persistence.Access; 
-import javax.persistence.AccessType; 
-import javax.persistence.Entity;
 
-@Entity 
-@Access(AccessType.PROPERTY) 
-public class Reviewer extends DomainEntity {
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.SafeHtml;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Reviewer extends Actor {
 
 	// Atributos
 	private String					keywords;
-	private Collection<Submission>					submissions;
+	private Collection<Submission>	submissions;
 
 
 	// Getters
 	@NotBlank
-	@SafeHtml(whitelistType = SafeHtml.WhiteListType.NONE) 
-	public String getKeywords(){
+	@SafeHtml(whitelistType = SafeHtml.WhiteListType.NONE)
+	public String getKeywords() {
 		return this.keywords;
 	}
 
 	@ManyToMany
-	public Collection<Submission> getSubmissions(){
+	public Collection<Submission> getSubmissions() {
 		return this.submissions;
 	}
 
-
-
 	// Setters
-	public void setKeywords(final String keywords){
-		this.keywords = keywords; 
+	public void setKeywords(final String keywords) {
+		this.keywords = keywords;
 	}
 
-	public void setSubmissions(final Collection<Submission> submissions){
-		this.submissions = submissions; 
+	public void setSubmissions(final Collection<Submission> submissions) {
+		this.submissions = submissions;
 	}
 
 }
--- AcmeConference/src/main/java/domain/Sponsor.java
+++ AcmeConference/src/main/java/domain/Sponsor.java
@@ -1,27 +1,27 @@
+
 package domain;
 
-import javax.persistence.Access; 
-import javax.persistence.AccessType; 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
 import javax.persistence.Entity;
 
-@Entity 
-@Access(AccessType.PROPERTY) 
-public class Sponsor extends DomainEntity {
+@Entity
+@Access(AccessType.PROPERTY)
+public class Sponsor extends Actor {
 
 	// Atributos
-	private CreditCard					creditcard;
+	private CreditCard	creditcard;
 
 
 	// Getters
-	public CreditCard getCreditcard(){
+
+	public CreditCard getCreditcard() {
 		return this.creditcard;
 	}
 
-
-
 	// Setters
-	public void setCreditcard(final CreditCard creditcard){
-		this.creditcard = creditcard; 
+	public void setCreditcard(final CreditCard creditcard) {
+		this.creditcard = creditcard;
 	}
 
 }
