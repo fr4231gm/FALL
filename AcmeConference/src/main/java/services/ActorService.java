e705fdec95628a900ba5716afd5df3612e1530f0
Fran <fr4231gm@gmail.com>
2019-07-06 13:43:35 +0200

    feat - configuration working

---
+++ AcmeConference/src/main/java/services/ActorService.java
@@ -0,0 +1,115 @@
+
+package services;
+
+import java.util.Collection;
+
+import javax.persistence.Inheritance;
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import repositories.ActorRepository;
+import security.LoginService;
+import security.UserAccount;
+import domain.Actor;
+
+@Service
+@Transactional
+@Inheritance
+public class ActorService {
+
+	// Managed repository -----------------------------------------------------
+
+	@Autowired
+	private ActorRepository			actorRepository;
+
+	// Supporting services ----------------------------------------------------
+
+	@Autowired
+	private AdministratorService	administratorService;
+
+	@Autowired
+	private ConfigurationService	configurationService;
+
+
+	//@Autowired
+	//private MessageService			messageService;
+
+	// Simple CRUDs methods ---------------------------------------------------
+
+	public Collection<Actor> findAll() {
+
+		Collection<Actor> result;
+
+		result = this.actorRepository.findAll();
+		Assert.notNull(result);
+
+		return result;
+
+	}
+
+	// Other business methods ---------------------------
+
+	public Actor findByPrincipal() {
+		Actor result;
+		UserAccount userAccount;
+
+		userAccount = LoginService.getPrincipal();
+		Assert.notNull(userAccount);
+
+		result = this.findByUserAccount(userAccount);
+		Assert.notNull(result);
+
+		return result;
+
+	}
+
+	public Actor findActorByUsername(final String username) {
+		Assert.notNull(username);
+		final Actor result;
+		result = this.actorRepository.findActorByUsername(username);
+		return result;
+	}
+
+	public Actor findByUserAccount(final UserAccount userAccount) {
+		Assert.notNull(userAccount);
+		final Actor result;
+
+		result = this.actorRepository.findActorByUserAccountId(userAccount.getId());
+
+		return result;
+
+	}
+
+	public Collection<Actor> findAllExceptPrincipal() {
+		Collection<Actor> result;
+		Actor principal;
+
+		result = this.actorRepository.findAll();
+		Assert.notNull(result);
+
+		principal = this.findByPrincipal();
+		Assert.notNull(principal);
+
+		result.remove(principal);
+		return result;
+	}
+
+	public Actor save(final Actor principal) {
+		Assert.notNull(principal);
+		return this.actorRepository.save(principal);
+
+	}
+
+	// UPDATE actors logged as administrator
+	public void update(final Actor actor) {
+		this.actorRepository.save(actor);
+	}
+	//Borrar actor
+	public void delete(final Actor principal) {
+		Assert.notNull(principal);
+		this.actorRepository.delete(principal);
+	}
+}
