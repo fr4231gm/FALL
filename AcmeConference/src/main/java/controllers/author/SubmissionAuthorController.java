15be6cb9b2971ec8d0e654160b000f92a016b319
Fran <fr4231gm@gmail.com>
2019-07-11 13:44:22 +0200

    wip

--- AcmeConference/src/main/java/controllers/author/SubmissionAuthorController.java
+++ AcmeConference/src/main/java/controllers/author/SubmissionAuthorController.java
@@ -3,8 +3,11 @@ package controllers.author;
 
 import java.util.Collection;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,6 +25,36 @@ public class SubmissionAuthorController extends AbstractController {
 	@Autowired
 	private SubmissionService	submissionService;
 
+
+	// Create
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
+	public ModelAndView create() {
+		ModelAndView res;
+		final Submission sub = this.submissionService.create();
+
+		res = this.createEditModelAndView(sub);
+
+		return res;
+	}
+
+	@RequestMapping(value = "/create", method = RequestMethod.POST, params = "save")
+	public ModelAndView save(@Valid final Submission s, final BindingResult binding) {
+		ModelAndView res;
+		Submission aux;
+
+		if (binding.hasErrors())
+			res = this.createEditModelAndView(s);
+		else
+			try {
+
+				aux = this.submissionService.save(s);
+				res = new ModelAndView("redirect:display.do?applicationId=" + aux.getId());
+
+			} catch (final Throwable oops) {
+				res = this.createEditModelAndView(s, "submission.commit.error");
+			}
+		return res;
+	}
 
 	//List
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
@@ -54,6 +87,23 @@ public class SubmissionAuthorController extends AbstractController {
 		res.addObject("submission", s);
 
 		return res;
+	}
+
+	// Ancillary metods
+	protected ModelAndView createEditModelAndView(final Submission s) {
+		ModelAndView result;
+		result = this.createEditModelAndView(s, null);
+		return result;
+	}
+
+	protected ModelAndView createEditModelAndView(final Submission s, final String messageCode) {
+		ModelAndView result;
+
+		result = new ModelAndView("submission/edit");
+		result.addObject("submission", s);
+		result.addObject("message", messageCode);
+
+		return result;
 	}
 
 }
--- AcmeConference/src/main/java/services/SubmissionService.java
+++ AcmeConference/src/main/java/services/SubmissionService.java
@@ -2,6 +2,7 @@
 package services;
 
 import java.util.Collection;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -39,4 +40,47 @@ public class SubmissionService {
 		return s;
 	}
 
+	public Submission create() {
+		final Submission s = new Submission();
+		s.setAuthor(this.authorService.findByPrincipal());
+		s.setTicker(this.generateTicker(s));
+		s.setStatus("UNDER-REVIEW");
+
+		return s;
+	}
+
+	private String generateTicker(final Submission s) {
+		String res = "";
+
+		final String alphanumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+		for (int i = 0; i < 4; i++) {
+			final int randomNumber = (int) Math.floor(Math.random() * (alphanumeric.length() - 1));
+			res += alphanumeric.charAt(randomNumber);
+			;
+		}
+		String middleName;
+		final String name = s.getAuthor().getName();
+		if (s.getAuthor().getMiddleName() != null)
+			middleName = s.getAuthor().getMiddleName();
+		else
+			middleName = "X";
+
+		final String surname = s.getAuthor().getSurname();
+
+		// Adding formatted date to alphanumeric code
+		res = name.charAt(0) + middleName.charAt(0) + surname.charAt(0) + "-" + res;
+
+		return res;
+	}
+
+	public Submission save(final Submission s) {
+		Submission result;
+		Assert.isTrue(s.getAuthor().getId() == this.authorService.findByPrincipal().getId());
+		final Date actual = new Date(System.currentTimeMillis() - 1);
+		Assert.isTrue(s.getConference().getSubmissionDeadline().after(actual));
+		s.setMoment(actual);
+		result = this.submissionRepository.save(s);
+		return result;
+	}
 }
