71d85f037139bdfd16b82fe12e822fd88ae4367f
Lucía <luciadelcarmenfuentes@gmail.com>
2019-08-31 18:11:26 +0200

    juju

--- AcmeConference/src/main/java/controllers/QuoletController.java
+++ AcmeConference/src/main/java/controllers/QuoletController.java
@@ -1,11 +1,34 @@
-
 package controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import domain.Quolet;
+
+import services.QuoletService;
 
 @Controller
 @RequestMapping("/quolet")
-public class QuoletController {
+public class QuoletController extends AbstractController {
 
+	@Autowired
+	private QuoletService quoletService;
+
+	@RequestMapping(value = "/display", method = RequestMethod.GET)
+	public ModelAndView display(@RequestParam final int quoletId) {
+		ModelAndView res;
+		Quolet quolet;
+
+		quolet = this.quoletService.findOne(quoletId);
+
+		res = new ModelAndView("quolet/display");
+		res.addObject("quolet", quolet);
+		res.addObject("conference", quolet.getConference());
+
+		return res;
+	}
 }
