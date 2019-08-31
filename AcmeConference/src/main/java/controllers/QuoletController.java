57afe8aa1a660780a8ae9d466f53b2b845190cdc
Lucía <luciadelcarmenfuentes@gmail.com>
2019-08-31 19:00:44 +0200

    fix - populadddd

--- AcmeConference/src/main/java/controllers/QuoletController.java
+++ AcmeConference/src/main/java/controllers/QuoletController.java
@@ -2,14 +2,16 @@ package controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import domain.Quolet;
-
+import services.AdministratorService;
 import services.QuoletService;
+import domain.Administrator;
+import domain.Quolet;
 
 @Controller
 @RequestMapping("/quolet")
@@ -17,6 +19,9 @@ public class QuoletController extends AbstractController {
 
 	@Autowired
 	private QuoletService quoletService;
+	
+	@Autowired
+	private AdministratorService administratorService;
 
 	@RequestMapping(value = "/display", method = RequestMethod.GET)
 	public ModelAndView display(@RequestParam final int quoletId) {
@@ -24,6 +29,11 @@ public class QuoletController extends AbstractController {
 		Quolet quolet;
 
 		quolet = this.quoletService.findOne(quoletId);
+		
+		if(quolet.getIsDraft().equals(false)){
+			Administrator principal = this.administratorService.findByPrincipal();
+			Assert.notNull(principal);
+		}
 
 		res = new ModelAndView("quolet/display");
 		res.addObject("quolet", quolet);
